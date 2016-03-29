3dc054ee7d526513e4245f36950da42002e52f40
Ben Calabrese <bencalabrese@gmail.com>
2016-03-29 16:24:15 -0700

    refactored new.html.erb

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -11,12 +11,46 @@ class PuppiesController < ApplicationController
   end
 
   def new
+    @puppy = Puppy.new
     render :new
   end
 
   def edit
     @puppy = Puppy.find(params[:id])
     render :edit
+  end
+
+  #We'll fix this stuff later
+  
+  # def create
+  #   @puppy = Puppy.new(puppy_params)
+  #
+  #   if @puppy.save
+  #     redirect_to @puppy
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  # def update
+  #   @puppy = Puppy.find(params[:id])
+  #
+  #   if @puppy.update(puppy_params)
+  #     redirect_to @puppy
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  private
+  def puppy_params
+    params.require(:puppy).permit(
+      :name,
+      :birth_date,
+      :color,
+      :sex,
+      :description
+    )
   end
 
 end

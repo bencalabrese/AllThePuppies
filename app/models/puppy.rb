69a21bba74cd62956a4c0a1ed5fa189aec2b2e43
Ben Calabrese <bencalabrese@gmail.com>
2016-03-29 11:09:19 -0700

    initial commit

---
+++ app/models/puppy.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: puppies
+#
+#  id          :integer          not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Puppy < ActiveRecord::Base
+end
